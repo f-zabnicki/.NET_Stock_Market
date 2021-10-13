79292d21575f8bbcc54bd9ffd19893a4aba98012
Filip Żabnicki <filip.zabnicki@globallogic.com>
2021-10-13 16:08:39 +0200

    initialize project

---
+++ Stock Market/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stock_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
